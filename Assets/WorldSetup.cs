e3dd47e1ee7676843cf226f02e5a0723f7ba608e
konradowc <konradowcz@gmail.com>
2023-02-17 23:25:40 -0800

    more

--- Assets/WorldSetup.cs
+++ Assets/WorldSetup.cs
@@ -6,15 +6,54 @@ public class WorldSetup : MonoBehaviour
 {
     [SerializeField] private GameObject house;
 
+    enum Rotation
+    {
+        LEFT, RIGHT, UP, DOWN
+    }
+
+    struct Coord
+    {
+        public float x;
+        public float z;
+
+        public Rotation rot;
+
+        public Coord(float intX, float intZ, Rotation rotation)
+        {
+            this.x = intX;
+            this.z = intZ;
+            this.rot = rotation;
+        }
+    }
+
     void Start()
     {
+        Coord[] houseLocs = new Coord[] {
+            new Coord(10, 10, Rotation.LEFT),
+            new Coord(40, 10, Rotation.LEFT),
+            new Coord(70, 10, Rotation.LEFT),
+            new Coord(110, 10, Rotation.LEFT),
+
+            new Coord(10, 50, Rotation.RIGHT),
+            new Coord(40, 50, Rotation.RIGHT),
+            new Coord(70, 50, Rotation.RIGHT),
+            new Coord(110, 50, Rotation.RIGHT),
+        };
+
         // generate houses on map
-        for (int i = 0; i < 5; i++)
+        GameObject go;
+        for(int i = 0; i < houseLocs.Length; i++)
         {
-            GameObject go = Instantiate(house, new Vector3(Random.Range(-50, 50), 0, Random.Range(-50, 50)), transform.rotation);
-            go.transform.localScale = new Vector3(10, 10, 10);
+            go = Instantiate(house, new Vector3(houseLocs[i].x, 15, houseLocs[i].z), transform.rotation);
+            go.transform.localScale = new Vector3(40, 40, 40);
+
+            if (houseLocs[i].rot == Rotation.RIGHT)
+            {
+                go.transform.Rotate(0.0f, 180.0f, 0.0f, Space.Self);
+                go.transform.localPosition = new Vector3(go.transform.position.x - 70, go.transform.position.y, go.transform.position.z);
+            }
         }
-        
+
     }
 
     // Update is called once per frame
